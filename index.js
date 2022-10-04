41f36b23dc7d1ccb71b746d10666560cb2dd1e8b
Jaspreet Kaur <kaurjaspreet9795@gmail.com>
2022-10-04 12:55:47 -0700

    commit tree d3f265583f5b9425bd43ca170d5de898c47cca5e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    fetch ("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name , 
+            email: email, 
+            
+    }),
+    })
+    .then (res=>res.json())
+    .then (data=> {
+        console.log(data);
+        document.body.innerHTML = data.id;
+    })
+
+    .catch ((e) => {
+        alert("error message");
+        console.log (e.message);
+        document.body.innerHTML =  e.message ; 
+    })
+}
+
+// submitData ()
